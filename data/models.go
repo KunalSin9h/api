2e73a6601026d7a15f10b55dd1dac8ba0829b279
Kunal Singh <kunalsin9h@gmail.com>
2023-08-12 15:28:04 +0530

    increase contest deadline for connecting to db

--- data/models.go
+++ data/models.go
@@ -22,7 +22,7 @@ type BlogDoc struct {
 }
 
 func (mdb *MongoDB) Connect(connString string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connString))
